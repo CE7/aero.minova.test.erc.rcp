dbae79954e04508b6309aeb527f3fbf118eaf843
Christopher Erlanger <erlanger@minova.com>
2021-01-13 09:34:40 +0100

    umbennenn des Projektes

---
+++ src/aero/minova/test/erc/rcp/parts/ContactGroupsPart.java
@@ -0,0 +1,22 @@
+ 
+package aero.minova.test.erc.rcp.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class ContactGroupsPart {
+	@Inject
+	public ContactGroupsPart() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
---
+++ src/aero/minova/test/erc/rcp/parts/ContactPart.java
@@ -0,0 +1,22 @@
+ 
+package aero.minova.test.erc.rcp.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class ContactPart {
+	@Inject
+	public ContactPart() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
